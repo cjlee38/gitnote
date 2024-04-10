54315501398519ab7ec68897b6705e927269dffc
cjlee38 <gptpem38@gmail.com>
2024-04-10 21:46:56 +0900

    feat: matches init,add,view arguments to handler functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod argument;
+mod handlers;
+mod file_utils;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = argument::build_cli();
+    let matches = cli.get_matches();
+
+    match matches.subcommand() {
+        Some(("init", _)) => {
+            if let Err(e) = handlers::init_notes() {
+                eprintln!("Error initializing notes directory: {}", e);
+            }
+        }
+        Some(("add", add_matches)) => {
+            if let Err(e) = handlers::add_note(
+                add_matches.get_one::<String>("file").expect("required").clone(),
+                add_matches.get_one::<String>("line").expect("required").clone(),
+                add_matches.get_one::<String>("message").expect("required").clone(),
+            ) {
+                eprintln!("Error adding note: {}", e);
+            }
+        }
+        Some(("view", view_matches)) => {
+            if let Err(e) = handlers::view_notes(
+                view_matches.get_one::<String>("file").expect("required").clone()
+            ) {
+                eprintln!("Error viewing notes: {}", e);
+            }
+        }
+        _ => {}
+    }
 }
